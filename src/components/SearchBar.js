06f0d841b93b894d64901ca3a4164e60759250c7
Michael Duberstein <mduberst@amazon.com>
2019-10-09 12:42:00 -0400

    Before Clip 91: Managing State

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,21 +1,35 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import {Feather} from '@expo/vector-icons';
+import { View, TextInput, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 const SearchBar = () => {
   return (
-    <View style={styles.background}>
-      <Feather name='search' size={30}/>
-      <Text>Search Bar</Text>
+    <View style={styles.backgroundStyle}>
+      <Feather name="search" style={styles.iconStyle} />
+      <TextInput style={styles.inputStyle} placeholder="Search" />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  background: {
+  backgroundStyle: {
+    marginTop: 10,
     backgroundColor: '#F0EEEE',
     height: 50,
     borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: 'row'
+    // alignItems: 'center' - narrows the area for the user to tab - bad
+  },
+  inputStyle: {
+    // borderColor: 'black', // used during the design stage
+    // borderWidth: 1,
+    flex: 1,
+    fontSize: 18
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: 'center',
     marginHorizontal: 15
   }
 });
