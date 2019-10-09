9d4df697a2814a0198a946f0164f7bb054d2166b
Michael Duberstein <mduberst@amazon.com>
2019-10-09 16:36:22 -0400

    Before Clip 101: FlatList Rendering

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
@@ -7,6 +7,14 @@ import ResultsList from '../components/ResultsList';
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [searchApi, results, errorMessage] = useResults();
+
+  // console.log(results);
+  const filterResultsByPrice = price => {
+    // price === '$' || '$$' || '$$$'
+    return results.filter(result => {
+      return result.price === price;
+    });
+  };
 
   return (
     <View>
@@ -20,9 +28,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
-      <ResultsList title="Cost Effective" />
-      <ResultsList title="Bit Pricier" />
-      <ResultsList title="Big Spender" />
+      <ResultsList results={filterResultsByPrice('$')} title='Cost Effective' />
+      <ResultsList results={filterResultsByPrice('$$')} title='Bit Pricier' />
+      <ResultsList results={filterResultsByPrice('$$$')} title='Big Spender' />
     </View>
   );
 };
