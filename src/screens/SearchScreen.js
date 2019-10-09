d40b1e23ccddb0fef91cfecb968d60e9320f1b00
Michael Duberstein <mduberst@amazon.com>
2019-10-09 13:12:34 -0400

    Before Section 9 Using Outside API

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,7 +6,11 @@ const SearchScreen = () => {
   const [term, setTerm] = useState('');
   return (
     <View>
-      <SearchBar term={term} onTermChange={newTerm => setTerm(newTerm)} />
+      <SearchBar
+        term={term}
+        onTermChange={newTerm => setTerm(newTerm)}
+        onTermSubmit={() => console.log('term was submitted')}
+      />
       <Text>Search Screen</Text>
       <Text>{term}</Text>
     </View>
