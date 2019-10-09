df0ce4754953dc15caf1030660cd1199f148a648
Michael Duberstein <mduberst@amazon.com>
2019-10-09 13:27:47 -0400

    Before Clip 94: Making the Request

---
+++ src/api/yelp.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'https://api.yelp.com/v3/businesses',
+  headers: {
+    Authorization: 'Bearer DeXPwWio49WRUnmJv_76exXJdcA0z4r5bBn1nqFyZo-A1_VCUbyKw1uD8OYRF2-Mlusz5bszEVbJPAr-MUr1AVPwsjR-9Gf0qnKWom6Zw-mm7sTL_WZwYpJFMvadXXYx'
+  }
+});
+
+
+
+// usage: yelp.get('/search');
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp'
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+
   return (
     <View>
       <SearchBar
